42567aacf5d15af0127eecb0af023029f57be34b
Leonard Burns <leonard4@gmail.com>
2014-07-27 14:54:42 -0700

    Added some comments, cleaned up old code
    Removed a few old commented chunks of code
    Added more comments
    Fixed some tabbing and spacing errors

--- Scripts/ChatScript.cs
+++ Scripts/ChatScript.cs
@@ -5,23 +5,31 @@ using System;
 
 public class ChatScript : MonoBehaviour {
 
+    // Window will start 5 over and at the bottom of the screen, minus the height of the window
+    // this puts it in the bottom left corner by default
     Rect windowSize = new Rect(5, Screen.height - 255, 375, 250);
-    bool windowLocked = false;
-    public Texture lockIcon;
-	public Texture unlockIcon;
+    // Can we currently move the window?
+	bool windowLocked = false;
+    
+    // Our textures for the lock and unlock icons
+	public Texture lockIcon;
+    public Texture unlockIcon;
 	
-
+    // Display the chat window?
     public bool showChat = true;
+    // Are we showing the time in the chat window?
     bool showChatTime = true;
 
-
+    // Vector2 for the scroll view sizing
     Vector2 scrollViewVector = Vector2.zero;
+    // Some initial text strings
     string innerText = "Inside the scrollview";
     string chatString = "Enter text here";
     string lastString = "Enter text here";
 
     class ChatEntry
     {
+        // Three elements to a chat message: Time, Name, and Text
         public DateTime timeStamp;
         public string name = "";
         public string text = "";
@@ -49,10 +57,12 @@ public class ChatScript : MonoBehaviour {
         }
     }
 
+    // New list for our chat entries
     List<ChatEntry> chatEntry = new List<ChatEntry>();
 
 	// Use this for initialization
 	void Start () {
+        // This will cause the chat window to be focused at start up
         //GUI.FocusControl("chatinput");
 	}
 	
@@ -63,18 +73,20 @@ public class ChatScript : MonoBehaviour {
 	
     void OnGUI()
     {
+        // Create our window
         windowSize = GUI.Window(0, windowSize, chatWindow, "Chat");
     }
 
     void chatWindow(int windowid)
     {
-
+        // Show chat if its enabled
         if (showChat)
         {
+            // Events for capturing mouse positions later for clicking lock/unlock
             Event e = Event.current;
             string tmpString = String.Empty;
 
-            // Where our window will be, and how big
+            // Where our lock/unlock texture will be, based off the window size
             Rect lockPosition = new Rect(windowSize.width - 20, 2, 15, 15);
             // Draw the lock/unlock icon based on the bool if its locked
             GUI.DrawTexture(lockPosition, windowLocked ? lockIcon : unlockIcon);
@@ -82,11 +94,13 @@ public class ChatScript : MonoBehaviour {
             // Check if we clicked on the lock icon
             if (lockPosition.Contains(e.mousePosition))
             {
+                // Event checking, if we left clicked AND it was a mouse AND it was mouse down, 
+                // then we toggle the lock/unlock state
                 if (e.button == 0 && e.isMouse && e.type == EventType.MouseDown)
                     windowLocked = !windowLocked;
             }
 
-            // Sets our textfield name
+            // Sets our textfield name so we can focus it later 
             GUI.SetNextControlName("chatinput");
             chatString = GUI.TextField(new Rect(2, windowSize.height - 32, windowSize.width - 4, 30), chatString, 400);
 
@@ -101,10 +115,11 @@ public class ChatScript : MonoBehaviour {
                 lastString = chatString;
                 GUI.FocusControl("chatinput");
             }
-
-            //if (Input.GetKeyDown(KeyCode.Return) )
+            
+            // Initial text string is not empty, and its not the first string then we do normal chat
             if (chatString != String.Empty && chatString != "Enter text here")
             {
+                // We've pressed enter, time to do stuff!
                 if (e.keyCode == KeyCode.Return)
                 {
                     // Add an entry to the list
@@ -126,7 +141,6 @@ public class ChatScript : MonoBehaviour {
                     tmpString += tmpChat.timeStamp + " " + tmpChat.name + " " + tmpChat.text + "\n";
                 else
                     tmpString += tmpChat.name + " " + tmpChat.text + "\n";
-
             }
 
             innerText = tmpString;
@@ -142,16 +156,19 @@ public class ChatScript : MonoBehaviour {
             GUI.EndScrollView();
         }
 
+        // Allow window to be draggable if its not locked
         if (!windowLocked)
             GUI.DragWindow();
     }
 
+    // Add an entry to our chatentry list
     void AddChatEntry(DateTime aTime, string aName, string aText)
     {
         ChatEntry tmpEntry = new ChatEntry(aTime, aName, aText);
         chatEntry.Add(tmpEntry);
     }
-
+    
+    // Check if our chat command had a slash in it "/"
     void parseChatCommand(string command)
     {
 		if(command.Length > 0)
